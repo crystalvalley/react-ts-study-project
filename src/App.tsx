64840687445dc0f6a29fd1be3d524404597cd8f1
ParkHyeokjoon <hamkegaja2@gmail.com>
2018-08-16 22:02:31 +0900

    component divide
    component divide

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,44 @@
-// ajax 통신을 위한 module
-import axios from 'axios';
 import * as React from 'react';
 import Divided from './Divided';
 
-// prop은 설정값 - 자신이 변경할 수 없는, 주어진 값
-// 사람이 성별이 남자다. => 불변함
-// state는 상태값
-// 사람이 배가 고프다. 밥을 먹으면 해소가 가능함 => 변할 수 있음
-
-// 상태의 모양
-interface IState {
-  // text라는 이름의 string 상태를 보유함
-  text: string;
-  updated: string;
+interface IState{
+  number : number;
 }
 
-// generic 규칙은 props,state 순서
-class App extends React.Component<{}, IState> {
-  // 생성자, props를 받아 생성함
+class App extends React.Component<{},IState> {
   constructor(props: {}) {
     super(props);
-    // state를 직접적으로 '=' 통해서 대입시키는 건
-    // constructor에서만
-    this.state = {
-      text: "초기 상태",
-      updated: "안 됨"
+    this.state={
+      number : 0
     }
     this.onClick = this.onClick.bind(this);
   }
 
-  // component가 update된 직후에 실행하는 메소드
-  // component내부의 속성이나 상태가 변경되는 것을 update라고 해요
-  public componentDidUpdate() {
-    if (this.state.updated === "됨") { return; }
-    // 밑의 메소드가 실행되면 무조건 update됨
-    this.setState({
-      updated: "됨"
-    })
-    // axios.메소드(주소,값들)
-    axios.get("https://www.naver.com", {
-      // parameter들
-      // spring에서 controller메소드들이 받는 parameter
-      params: {
-        title: "test"
-      }
-    })
-      // $.ajax에서 success
-      // 통신의 결과로 어떠한 함수를 실행함
-      // block is empty에러가 뜰테니 주석만 처리
-      // .then((response) => {})
-  }
-
   public render() {
-    // JSX문법상 HTML부분은 주석을 이하와 같이 처리함
     return (
       <div>
-        {/* 이렇게 */}
-        {/* 일단은 component로 알고 ㄱㄱ */}
-        {/* 밑의 div Component의 내부에 state.text가 들어가있음 */}
+        {/* 1단계 분리할 부분을 체크 */}
+        {/* 4단계 분리시킬 컴포넌트가 가져야할 속성을 파악 */}
         <div>
-          {this.state.text}
+          {/* div안에 number라는 속성이 있음 */}
+          {this.state.number}
         </div>
-        {/* 바로 위의 div와 동일한 Component를 가져옴 */}
-        {/* this.state.text라는 값을 밑의 Divided에 넣어줘야 함 */}
-        <Divided
-          text={this.state.text}
-        />
-        <div>
-          {this.state.updated}
-        </div>
-        <button
-          onClick={this.onClick}
-        >
-          이것은 버튼
+        {/* button안에 onClick이라는 속성이 있음 */}
+        <button onClick={this.onClick}>
+          button
         </button>
+        <Divided
+          recievedNumber={this.state.number}
+          recievedOnClick={this.onClick}
+        />
       </div>
     );
   }
-  private onClick() {
-    // state를 변경하겠다.
-    // immutable
+  private onClick(){
+    const num = this.state.number
     this.setState({
-      text: "변경됨"
+      number : num+1
     })
   }
 }
-
 export default App;
--- src/Divided.tsx
+++ src/Divided.tsx
@@ -1,24 +1,29 @@
-import * as React from 'react';
-
-
-// 주어지는 속성의 형태
+// 2단계 분리된 컴포넌트를 생성
+import * as React from 'react'
 interface IProps{
-    // text라는 이름을 가진 string 속성을 부여받음
-    text : string;
+    recievedNumber : number;
+    // recievedOnClick():void;
+    recievedOnClick:()=>void;
+    
 }
-// human이란 component를 만든다.
-// 태어나면 무조건 성별이란 속성을 부여받듯이
-// 이러한 컴포넌트들은 props가 반드시 부여되어야 한다.
-
 export default class Divided extends React.Component<IProps> {
     constructor(props:IProps){
         super(props);
     }
     public render() {
         return (
-            <div>
-                {this.props.text}
-            </div>
+            <React.Fragment>
+                {/* return은 무조건 최상위 태그 하나로만 나와야한다.*/}
+                {/* 3단계 복사 */}
+                < div >
+                    {/* div안에 number라는 속성이 있음 */}
+                    {this.props.recievedNumber}
+                </div >
+                {/* button안에 onClick이라는 속성이 있음 */}
+                <button onClick={this.props.recievedOnClick}>
+                    button
+                </button>
+            </React.Fragment>
         );
     }
 }
